d67c24f0a8fc3990cf23e3a6b2d91a2a2833999c
PheRonaldo9 <105642892+PheRonaldo9@users.noreply.github.com>
2024-05-13 15:24:14 +0300

    Update universe.sql

--- universe.sql
+++ universe.sql
@@ -16,29 +16,6 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-DROP DATABASE universe;
---
--- Name: universe; Type: DATABASE; Schema: -; Owner: freecodecamp
---
-
-CREATE DATABASE universe WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
-
-
-ALTER DATABASE universe OWNER TO freecodecamp;
-
-\connect universe
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
@@ -160,6 +137,42 @@ ALTER SEQUENCE public.planet_planet_id_seq OWNED BY public.planet.planet_id;
 
 
 --
+-- Name: space_mission; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.space_mission (
+    name character varying(50) NOT NULL,
+    launch_date date NOT NULL,
+    space_mission_id integer NOT NULL,
+    moon_id integer NOT NULL
+);
+
+
+ALTER TABLE public.space_mission OWNER TO freecodecamp;
+
+--
+-- Name: space_mission_space_mission_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.space_mission_space_mission_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.space_mission_space_mission_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: space_mission_space_mission_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.space_mission_space_mission_id_seq OWNED BY public.space_mission.space_mission_id;
+
+
+--
 -- Name: star; Type: TABLE; Schema: public; Owner: freecodecamp
 --
 
@@ -221,6 +234,13 @@ ALTER TABLE ONLY public.planet ALTER COLUMN planet_id SET DEFAULT nextval('publi
 
 
 --
+-- Name: space_mission space_mission_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.space_mission ALTER COLUMN space_mission_id SET DEFAULT nextval('public.space_mission_space_mission_id_seq'::regclass);
+
+
+--
 -- Name: star star_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
 --
 
@@ -231,70 +251,89 @@ ALTER TABLE ONLY public.star ALTER COLUMN star_id SET DEFAULT nextval('public.st
 -- Data for Name: galaxy; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.galaxy VALUES (1, 'Milky Way', 'Spiral', 100, true, 100000, 13.6);
-INSERT INTO public.galaxy VALUES (2, 'Andromeda', 'Spiral', 1000, false, 220000, 10.3);
-INSERT INTO public.galaxy VALUES (3, 'Triangulum', 'Spiral', 40, false, 60000, 10.0);
-INSERT INTO public.galaxy VALUES (4, 'Messier 87', 'Elliptical', 500, true, 150000, 13.2);
-INSERT INTO public.galaxy VALUES (5, 'Whirlpool', 'Spiral', 100, false, 60000, 400.0);
-INSERT INTO public.galaxy VALUES (6, 'Sombrero', 'Spiral', 1000, false, 50000, 13.0);
+COPY public.galaxy (galaxy_id, name, galaxy_type, no_of_stars_in_billion, active_galactic_nucleus, diameter_in_light_years, age_in_billion_years) FROM stdin;
+1	Milky Way	Spiral	100	t	100000	13.6
+2	Andromeda	Spiral	1000	f	220000	10.3
+3	Triangulum	Spiral	40	f	60000	10.0
+4	Messier 87	Elliptical	500	t	150000	13.2
+5	Whirlpool	Spiral	100	f	60000	400.0
+6	Sombrero	Spiral	1000	f	50000	13.0
+\.
 
 
 --
 -- Data for Name: moon; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.moon VALUES (1, true, 'Io', 1.8, 5, -150);
-INSERT INTO public.moon VALUES (2, true, 'Europa', 3.2, 5, -100);
-INSERT INTO public.moon VALUES (3, true, 'Ganymede', 7.2, 5, -150);
-INSERT INTO public.moon VALUES (4, true, 'Callisto', 16.7, 5, -100);
-INSERT INTO public.moon VALUES (5, false, 'Amalthea', 0.5, 5, -200);
-INSERT INTO public.moon VALUES (6, false, 'Thebe', 0.7, 5, -150);
-INSERT INTO public.moon VALUES (7, false, 'Metis', 1.1, 5, -100);
-INSERT INTO public.moon VALUES (8, false, 'Adrastea', 0.3, 5, -150);
-INSERT INTO public.moon VALUES (9, false, 'Themisto', 129.9, 5, -100);
-INSERT INTO public.moon VALUES (10, false, 'Carme', 693.2, 5, -150);
-INSERT INTO public.moon VALUES (11, false, 'Titan', 15.9, 6, -100);
-INSERT INTO public.moon VALUES (12, false, 'Enceladus', 1.4, 6, -150);
-INSERT INTO public.moon VALUES (13, false, 'Dione', 2.7, 6, -100);
-INSERT INTO public.moon VALUES (14, false, 'Rhea', 4.5, 6, -150);
-INSERT INTO public.moon VALUES (15, false, 'Iapetus', 79.3, 6, -100);
-INSERT INTO public.moon VALUES (16, false, 'Miranda', 1.4, 7, -150);
-INSERT INTO public.moon VALUES (17, false, 'Ariel', 2.5, 7, -100);
-INSERT INTO public.moon VALUES (18, false, 'Umbriel', 4.1, 7, -150);
-INSERT INTO public.moon VALUES (19, false, 'Titania', 8.7, 7, -100);
-INSERT INTO public.moon VALUES (20, false, 'Oberon', 13.5, 7, -150);
-INSERT INTO public.moon VALUES (21, true, 'Moon', 27.3, 3, -150);
-INSERT INTO public.moon VALUES (22, false, 'Phobos', 0.3, 4, -100);
+COPY public.moon (moon_id, tidally_locked, name, orbital_period_in_earth_days, planet_id, surface_temperature_in_degree_celcius) FROM stdin;
+1	t	Io	1.8	5	-150
+2	t	Europa	3.2	5	-100
+3	t	Ganymede	7.2	5	-150
+4	t	Callisto	16.7	5	-100
+5	f	Amalthea	0.5	5	-200
+6	f	Thebe	0.7	5	-150
+7	f	Metis	1.1	5	-100
+8	f	Adrastea	0.3	5	-150
+9	f	Themisto	129.9	5	-100
+10	f	Carme	693.2	5	-150
+11	f	Titan	15.9	6	-100
+12	f	Enceladus	1.4	6	-150
+13	f	Dione	2.7	6	-100
+14	f	Rhea	4.5	6	-150
+15	f	Iapetus	79.3	6	-100
+16	f	Miranda	1.4	7	-150
+17	f	Ariel	2.5	7	-100
+18	f	Umbriel	4.1	7	-150
+19	f	Titania	8.7	7	-100
+20	f	Oberon	13.5	7	-150
+21	t	Moon	27.3	3	-150
+22	f	Phobos	0.3	4	-100
+\.
 
 
 --
 -- Data for Name: planet; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.planet VALUES (1, false, 'Mercury', 'Terrestrial', 0, 800, 6);
-INSERT INTO public.planet VALUES (2, false, 'Venus', 'Terrestrial', 0, 450, 6);
-INSERT INTO public.planet VALUES (3, true, 'Earth', 'Terrestrial', 1, 15, 6);
-INSERT INTO public.planet VALUES (4, false, 'Mars', 'Terrestrial', 2, -60, 6);
-INSERT INTO public.planet VALUES (5, false, 'Jupiter', 'Gas giant', 79, -150, 6);
-INSERT INTO public.planet VALUES (6, false, 'Saturn', 'Gas giant', 82, -178, 6);
-INSERT INTO public.planet VALUES (7, false, 'Uranus', 'Ice giant', 27, -214, 6);
-INSERT INTO public.planet VALUES (8, false, 'Neptune', 'Ice giant', 14, -201, 6);
-INSERT INTO public.planet VALUES (9, false, 'Ceres', 'Dwarf', 0, -106, 6);
-INSERT INTO public.planet VALUES (10, false, 'Pluto', 'Dwarf', 5, -240, 6);
-INSERT INTO public.planet VALUES (11, false, 'Haumea', 'Dwarf', 2, -119, 6);
-INSERT INTO public.planet VALUES (12, false, 'Makemake', 'Dwarf', 1, -239, 6);
+COPY public.planet (planet_id, habitable, name, planet_type, no_of_moons, surface_temperature_in_degree_celcius, star_id) FROM stdin;
+1	f	Mercury	Terrestrial	0	800	6
+2	f	Venus	Terrestrial	0	450	6
+3	t	Earth	Terrestrial	1	15	6
+4	f	Mars	Terrestrial	2	-60	6
+5	f	Jupiter	Gas giant	79	-150	6
+6	f	Saturn	Gas giant	82	-178	6
+7	f	Uranus	Ice giant	27	-214	6
+8	f	Neptune	Ice giant	14	-201	6
+9	f	Ceres	Dwarf	0	-106	6
+10	f	Pluto	Dwarf	5	-240	6
+11	f	Haumea	Dwarf	2	-119	6
+12	f	Makemake	Dwarf	1	-239	6
+\.
+
+
+--
+-- Data for Name: space_mission; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+COPY public.space_mission (name, launch_date, space_mission_id, moon_id) FROM stdin;
+Apollo 11	1969-07-16	1	21
+Apollo 15	1971-07-26	2	21
+Lunar Reconnaissance Orbiter	2009-06-18	3	21
+\.
 
 
 --
 -- Data for Name: star; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.star VALUES (1, true, 'Orion', 'G2V', 5778, 4, 5, 'Sun');
-INSERT INTO public.star VALUES (2, false, 'Ursa Major', 'K0III', 4400, 6, 2, 'Arcturus');
-INSERT INTO public.star VALUES (3, true, 'Cygnus', 'F8V', 6100, 2, 3, 'Kepler-22');
-INSERT INTO public.star VALUES (4, true, 'Draco', 'M1V', 3600, 8, 1, 'Proxima Centauri');
-INSERT INTO public.star VALUES (5, true, 'Lyra', 'M4V', 3200, 9, 4, 'Vega');
-INSERT INTO public.star VALUES (6, true, 'Sculptor', 'K2III', 4500, 10, 6, 'Alpha Sculptoris');
+COPY public.star (star_id, exoplanethost, constellation, spectral_class, temperature_in_degree_celcius, age_in_billion_years, galaxy_id, name) FROM stdin;
+1	t	Orion	G2V	5778	4	5	Sun
+2	f	Ursa Major	K0III	4400	6	2	Arcturus
+3	t	Cygnus	F8V	6100	2	3	Kepler-22
+4	t	Draco	M1V	3600	8	1	Proxima Centauri
+5	t	Lyra	M4V	3200	9	4	Vega
+6	t	Sculptor	K2III	4500	10	6	Alpha Sculptoris
+\.
 
 
 --
@@ -316,6 +355,13 @@ SELECT pg_catalog.setval('public.moon_moon_id_seq', 1, false);
 --
 
 SELECT pg_catalog.setval('public.planet_planet_id_seq', 1, false);
+
+
+--
+-- Name: space_mission_space_mission_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.space_mission_space_mission_id_seq', 3, true);
 
 
 --
@@ -342,6 +388,14 @@ ALTER TABLE ONLY public.galaxy
 
 
 --
+-- Name: moon moon_name_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.moon
+    ADD CONSTRAINT moon_name_key UNIQUE (name);
+
+
+--
 -- Name: moon moon_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
 --
 
@@ -363,6 +417,30 @@ ALTER TABLE ONLY public.planet
 
 ALTER TABLE ONLY public.planet
     ADD CONSTRAINT planet_pkey PRIMARY KEY (planet_id);
+
+
+--
+-- Name: space_mission space_mission_mission_name_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.space_mission
+    ADD CONSTRAINT space_mission_mission_name_key UNIQUE (name);
+
+
+--
+-- Name: space_mission space_mission_name_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.space_mission
+    ADD CONSTRAINT space_mission_name_key UNIQUE (name);
+
+
+--
+-- Name: space_mission space_mission_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.space_mission
+    ADD CONSTRAINT space_mission_pkey PRIMARY KEY (space_mission_id);
 
 
 --
@@ -403,6 +481,14 @@ ALTER TABLE ONLY public.moon
 
 ALTER TABLE ONLY public.planet
     ADD CONSTRAINT planet_star_id_fkey FOREIGN KEY (star_id) REFERENCES public.star(star_id);
+
+
+--
+-- Name: space_mission space_mission_moon_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.space_mission
+    ADD CONSTRAINT space_mission_moon_id_fkey FOREIGN KEY (moon_id) REFERENCES public.moon(moon_id);
 
 
 --
